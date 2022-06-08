220dcdf384845a5379b80c268e3e4f3886430916
PaGenaro <80373174+PaGenaro@users.noreply.github.com>
2022-06-08 20:51:11 -0300

    Teste com javaScript

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let nome = "Paloma";
+let numero = 10;
+let ano = 2022 - numero;
+let soma = 2022 - 10;
+let templateString = "Estou mexendo no console.log";
+let quartaFeira = true;
+
+console.log(nome);
+console.log(soma);
+console.log("Estou mexendo no console.log");
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
